542e86a3c64295cf75aca9dbfa222cd2ebd5891c
adarsh <gkishan179@gmail.com>
2024-01-06 17:52:55 +0530

    initial commit

---
+++ src/components/main.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { NavLink } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+const Home = () => {
+  return (
+    <>
+      {/* <ul style={{ listStyleType: 'none', padding: '10px', display: 'flex', justifyContent: 'center', alignContent: "center" }}>
+        <li style={{ marginRight: '25px' }}><h4>SHOP</h4></li>
+        <li style={{ marginRight: '25px' }}><h4>SKILLS</h4></li>
+        <li style={{ marginRight: '25px' }}><h4>STORIES</h4></li>
+        <li style={{ marginRight: '25px' }}><h4>ABOUT</h4></li>
+        <li style={{ marginRight: '25px' }}><h4>CONTACT </h4></li>
+      </ul> */}
+      {/* </div> */}
+      {/* <img
+            className="card-img img-fluid"
+            src="./assets/main.png.jpg"
+            alt="Card"
+            height={500}
+          />
+          <div className="card-img-overlay d-flex align-items-center">
+            <div className="container">
+              <h5 className="card-title fs-1 text fw-lighter">New Season Arrivals</h5>
+              <p className="card-text fs-5 d-none d-sm-block ">
+                Discover your products
+              </p> */}
+      {/* </div>
+          </div> */}
+      {/* </div>
+      </div> */}
+    </>
+  );
+};
+
+export default Home;
